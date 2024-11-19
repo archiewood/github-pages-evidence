3a965b2adba0cffeed43664d620aa7ba15dba06c
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-11-19 18:28:38 -0500

    fix properly

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
-	import '@evidence-dev/tailwind/fonts.css';
-	import '../app.css';
-	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
-	export let data;
+  import "@evidence-dev/tailwind/fonts.css";
+  import "../app.css";
+  import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+  export let data;
 </script>
 
-<EvidenceDefaultLayout {data} githubRepo="https://github.com/archiewood/github-pages-evidence" />
-	<slot slot="content" />
+<EvidenceDefaultLayout
+  {data}
+  githubRepo="https://github.com/archiewood/github-pages-evidence"
+>
+  <slot slot="content" />
 </EvidenceDefaultLayout>
